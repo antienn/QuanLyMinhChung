c7bfbf798f5225f1fc3dc399a7b00e57257a46da
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2023-04-07 15:27:36 +0700

    Merge branch 'main' of https://github.com/supertien12/QuanLyMinhChung

--- src/QuanLyMinhChung/MinhChung/QuanLiMinhChung.java
+++ src/QuanLyMinhChung/MinhChung/QuanLiMinhChung.java
@@ -1,18 +1,2 @@
-package QuanLyMinhChung.MinhChung;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class QuanLiMinhChung {
-    private ArrayList<MinhChung> danhsachMinChung = new ArrayList<>();
-
-    //Ham them Minh CHung
-    public void addMinhChung(MinhChung ...dsMinhChung){
-        this.danhsachMinChung.addAll(Arrays.asList(dsMinhChung));
-    }
-    // Ham hien thi MinhChung
-    public void displayMinhChung(){
-        this.danhsachMinChung.forEach(x -> System.out.println(x));
-    }
-
+package QuanLyMinhChung.MinhChung;public class QuanLiMinhChung {
 }
